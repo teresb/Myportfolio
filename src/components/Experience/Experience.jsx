d30d616d2c7bfd9ca25e5bed24e834c73bddc744
teresb <theresechikodi2004@gmail.com>
2023-06-04 18:14:40 -0700

    updated code

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,8 +1,91 @@
 import React from 'react'
+import './experience.css'
+import { BsFillPatchCheckFill } from 'react-icons/bs'
 
-function Experience(){
-    return (
-        <div>Experience</div>
+const Experience = () => {
+  return (
+    <section id='experience'>
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className='experience__frontend'>
+          <h3>Photo Editing</h3>
+
+          <div className='experience__content'>
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Adobe Photoshop</h4>
+                <small className='text-light'>Intermediate</small>  
+             </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Adobe Lightroom</h4>
+               <small className='text-light'>Experienced</small>
+             </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Canva</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Picsart</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+
+        <div className='experience__backend'>
+        <h3>Video Editing</h3>
+          <div className='experience__content'>
+
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Adobe Premiere Pro</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>CapCut</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>VN</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>InShot</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
     )
 }
 
