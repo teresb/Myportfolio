b2a94c78a7e0a707b19956c4ef0309f646ec600d
teresb <theresechikodi2004@gmail.com>
2023-06-05 13:54:46 -0700

    updated header section

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
-import CV from '.././../assets/CV.pdf'
+import CV from '../../assets/CV.pdf'
+
 const CTA = () => {
-return ( <div className='cta'> <a href={CV} download className='btn'>Download CV</a> <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+return ( 
+<div className='cta'> 
+<a href={CV} download className='btn'>Download CV</a> 
+<a href="#contact" className='btn btn-primary'>Let's Talk</a>
 </div> ) }
 export default CTA
