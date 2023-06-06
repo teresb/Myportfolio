b979f9d03abf9d6d9ee18c73f56825045ebbdf93
teresb <theresechikodi2004@gmail.com>
2023-06-05 17:38:12 -0700

    fixed about component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -25,19 +25,19 @@ const About = () => {
             <article className='about__card'>
               <FaAward className='about__icon'/>
               <h5>Experience</h5>
-              <small>3+ Years Working</small>
+              <small>2+ Years Working</small>
             </article>
 
             <article className='about__card'>
               <FiUsers className='about__icon'/>
               <h5>Clients</h5>
-              <small>50+ Nationwide</small>
+              <small>20+ Nationwide</small>
             </article>
 
             <article className='about__card'>
               <VscFolderLibrary className='about__icon'/>
               <h5>Projects</h5>
-              <small>30+ Completed Projects</small>
+              <small>10+ Completed Projects</small>
             </article>
           </div>
           <p>
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -4,8 +4,6 @@ import IMG1 from '../../assets/portfolio1.jpg'
 import IMG2 from '../../assets/portfolio2.jpg'
 import IMG3 from '../../assets/portfolio3.jpg'
 import IMG4 from '../../assets/portfolio4.jpg'
-import IMG5 from '../../assets/portfolio5.png'
-import IMG6 from '../../assets/portfolio6.jpg'
 
 const data = [
   {
@@ -33,20 +31,6 @@ const data = [
     id: 4,
     image: IMG4,
     title: 'Maintaining tasks and tracking progress',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/Alien_pixels'
-  },
-  {
-    id: 5,
-    image: IMG5,
-    title: 'Charts template & infographics in Figma',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/Alien_pixels'
-  },
-  {
-    id: 6,
-    image: IMG6,
-    title: 'Charts template & infographics in Figma ',
     github: 'https://github.com',
     demo: 'https://dribbble.com/Alien_pixels'
   },
